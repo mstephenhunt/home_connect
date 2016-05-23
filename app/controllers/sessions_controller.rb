fff5988d1fd80254c08600dd8bb22f3d1f313b6c
Matt H <mstephenhunt@gmail.com>
2016-05-23 14:12:44 +0000

    Cleans up code for logging in. The authentication code has been moved a method in the user object. Error messages are more easily set on the calling object that way and the code is better segmented.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,51 +3,25 @@ class SessionsController < ApplicationController
   
   def new
     if logged_in?
-      redirect_to user_url(current_user)
+      redirect_to current_user
     end
   end
   
   def create
-    # Create bools used to determine if the user can log in
-    # and @user to hold potential error messages
-    user = nil; auth = nil;
     @user = User.new
+    @user.find_and_auth_user params[:session][:email].downcase, params[:session][:password]
     
-    # Log in. If it's unable, will set error messages on @user
-    log_in_and_auth(user, auth)
-    
-    if user && auth
-      # Log in and redirect
-      log_in(user)
-      redirect_to user
+    if !@user.errors.any?
+      log_in @user
+      redirect_to current_user
     else
-      # Error, re-render new
       render 'new'
     end
   end
+  
   
   def destroy
     log_out
     redirect_to root_url
   end
-  
-  ################################# Private ###########################
-  
-  private
-    def log_in_and_auth(user, auth)
-      # Attempt to find the user based on email provided
-      user = User.find_by(email: params[:session][:email].downcase)
-      
-      # If you've found the user, next try to authenticate them
-      if user
-        auth = user.authenticate(params[:session][:password])
-      end
-      
-      # Set any error messages
-      if !user
-        @user.errors.add :email, params[:session][:email].downcase + ' not found'
-      elsif !auth && !auth.nil?
-        @user.errors.add :password
-      end
-    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,32 @@ class User < ActiveRecord::Base
     
     has_many :plugs, dependent: :destroy
     
+    has_secure_password
     validates   :name, presence: true
     validates   :email, presence: true,
                 uniqueness: true
                 
-    has_secure_password
+    def find_and_auth_user(username, password)
+        user = User.find_by(email: username)
+        auth = false
+        
+        # If you found the user, attempt to authenticate
+        if user
+            auth = user.authenticate(password)
+        end
+        
+        # If there are errors, set them. Otherwise, set id on self
+        if !user
+            self.errors.add :email, username + ' not found'
+        elsif !auth
+            self.errors.add :password
+        else
+            self.id = user.id
+        end
+    end
+    
+    
+    ############################### Private #############################    
     
     private
         def before_save_user
