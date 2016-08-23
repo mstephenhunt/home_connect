43e77dd7c95fa72b04ee6f85132d2596bf59a87b
Matt Hunt <mstephenhunt@gmail.com>
2016-08-23 16:28:14 +0000

    Updates sessions_controller_spec to reference new UI features

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,27 +5,27 @@ describe SessionsController do
     describe '.new()', type: :feature do
         it 'gets the new template if not logged in' do
             visit '/sessions/new'
-            expect(page).to have_content('Log in')
+            expect(page).to have_css('#jumbotron_logo')
             
             visit '/'
-            expect(page).to have_content('Log in')
+            expect(page).to have_css('#jumbotron_logo')
         end
         
         it 'redirects to user#show if logged in' do
             @user = FactoryGirl.create(:user, 
                      password: "password", password_confirmation: "password")
             visit '/sessions/new'
-            fill_in 'Email', with: @user.email
-            fill_in 'Password', with: "password"
+            fill_in 'session_email', with: @user.email
+            fill_in 'session_password', with: "password"
             click_button 'Log in'
             
-            expect(page).to have_content("User: " + @user.name)
+            expect(page).to have_content(@user.name)
             
             visit '/'
-            expect(page).to have_content("User: " + @user.name)
+            expect(page).to have_content(@user.name)
             
             visit '/sessions/new'
-            expect(page).to have_content("User: " + @user.name)
+            expect(page).to have_content(@user.name)
         end
     end
     
@@ -37,19 +37,19 @@ describe SessionsController do
         end
         
         it 'prevents a non-existant user from logging in' do
-            fill_in 'Email', with: @user.email
-            fill_in 'Password', with: "password"
+            fill_in 'session_email', with: @user.email
+            fill_in 'session_password', with: "password"
             click_button 'Log in'
             
-            expect(page).to have_content("Log in")
+            expect(page).to have_css('#jumbotron_logo')
         end
         it 'prevents a user with a bad password from logging in' do
             @user.save
-            fill_in 'Email', with: @user.email
-            fill_in 'Password', with: "bad_password"
+            fill_in 'session_email', with: @user.email
+            fill_in 'session_password', with: "bad_password"
             click_button 'Log in'
             
-            expect(page).to have_content("Log in")
+            expect(page).to have_css('#jumbotron_logo')
         end
     end
     
@@ -58,20 +58,20 @@ describe SessionsController do
             @user = FactoryGirl.create(:user, 
                      password: "password", password_confirmation: "password")
             visit '/'
-            fill_in 'Email', with: @user.email
-            fill_in 'Password', with: "password"
+            fill_in 'session_email', with: @user.email
+            fill_in 'session_password', with: "password"
             click_button 'Log in'
 
             click_link 'Log out'
         end
         
         it 'log out redirects to the home page' do
-            expect(page).to have_content("Log in")
+            expect(page).to have_css('#jumbotron_logo')
         end
         
         it 'prevents logged out from going to their show page' do
             visit '/users/' + @user.id.to_s
-            expect(page).to have_content("Log in")
+            expect(page).to have_css('#jumbotron_logo')
         end
     end
 end
